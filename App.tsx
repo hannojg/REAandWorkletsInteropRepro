1ddce56c30239b906cdf8aed2d9fd04e114fc8bc
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-02-06 10:25:27 +0100

    Revert "explicit example"
    This reverts commit 050d62db60fdebfdab89a0b67745e337bed92a1a.

--- App.tsx
+++ App.tsx
@@ -1,41 +1,27 @@
 import React from 'react';
-import {Button, StyleSheet} from 'react-native';
-import Animated, {
-  runOnUI,
-  useAnimatedStyle,
-  useSharedValue,
-} from 'react-native-reanimated';
-import {Worklets} from 'react-native-worklets-core';
+import {Button, StyleSheet, View} from 'react-native';
+import {useSharedValue} from 'react-native-reanimated';
+import {useWorklet} from 'react-native-worklets-core';
 
 export default function App() {
-  const color = useSharedValue('white');
+  const sharedValue = useSharedValue(0);
 
-  const updateSharedValueFromWorklet = () => {
-    'worklet';
-    color.value = ['red', 'blue', 'green'][Math.floor(Math.random() * 3)];
-  };
-
-  const workletCoreFct = Worklets.createRunInContextFn(
-    updateSharedValueFromWorklet,
+  const updateSharedValueFromWorklet = useWorklet(
+    'default',
+    () => {
+      'worklet';
+      sharedValue.value = Math.random();
+    },
+    [sharedValue],
   );
 
-  const onPress = () => {
-    runOnUI(workletCoreFct)();
-  };
-
-  const animBackgroundStyle = useAnimatedStyle(() => {
-    return {
-      backgroundColor: color.value,
-    };
-  }, [color]);
-
   return (
-    <Animated.View style={[styles.centerContent, animBackgroundStyle]}>
+    <View>
       <Button
-        onPress={onPress}
+        onPress={updateSharedValueFromWorklet}
         title="Update shared value from different worklet"
       />
-    </Animated.View>
+    </View>
   );
 }
 
